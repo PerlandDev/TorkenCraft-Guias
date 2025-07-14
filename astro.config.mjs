e4d3d9547dff804dbaa4c39db7d9d8704a1cfcc0
EstoesMoises <contactomoises1@gmail.com>
2025-07-14 20:33:33 +0100

    guias iniciales

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,22 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Guías de TorkenCraft',
+			logo: {src: '/src/assets/torkencraft.png'},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Facciones',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Introducción', slug: 'facciones' },
+						{ label: 'Fundamentos', slug: 'facciones/fundamentos'},
+						{ label: 'Poder y Territorio', slug: 'facciones/poder-territorio'},
+						{ label: 'Diplomacia', slug: 'facciones/diplomacia'},
+						{ label: 'Gestión de Miembros', slug: 'facciones/gestion-miembros'},
+						{ label: 'Guerra y Combate', slug: 'facciones/guerra'},
+						{ label: 'Comandos', slug: 'facciones/comandos'}
 					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
